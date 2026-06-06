91e655b5cc9d8e84eebf8cfe29f7da4f40e369c8
Daggubati Kavya <daggubatikavya18@gmail.com>
2026-06-06 22:56:27 +0530

    Fixed backend route import

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const contactRoutes = require("./routes/contactRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+
 require("dotenv").config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/projects", projectRoutes);
+app.use("/api/contact", contactRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
@@ -20,6 +29,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const projectRoutes = require("./routes/projectRoutes");
-app.use("/api/projects", projectRoutes);
-app.use("/api/contact", contactRoutes);
